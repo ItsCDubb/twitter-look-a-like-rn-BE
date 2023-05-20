e47d5f99c01e19d2b81ee73a9ca267f203243b36
Chris Warren <chris.warren.2019@gmail.com>
2023-05-20 13:54:29 -0400

    Tweet CRUD & updated User CRUD

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -31,10 +31,13 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   const user = await prisma.user.findUnique({ where: { id: Number(id) } });
+  if (!user) {
+    return res.status(404).json({ error: "User not found!" });
+  }
   res.json(user);
 });
 // Update user
-// curl command: curl -X PUT -H 'Content-Type: application/json' -d '{\"name\": \"Chris\", \"bio\": \"Hello!\"}' http://localhost:3000/user/1
+// curl command: curl -X PUT -H 'Content-Type: application/json' -d '{\"name\": \"Chris\", \"bio\": \"Hello!\"}' http://localhost:3000/user/id
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
   const { bio, name, image } = req.body;
